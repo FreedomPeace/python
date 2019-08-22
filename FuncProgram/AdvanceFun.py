b90c199520f35573f566c0368fbb0c732a37b2c1
FreedomPeace <18658251543@163.com>
2019-08-22 22:44:32 +0800

    函数编程-高阶函数map

---
+++ FuncProgram/AdvanceFun.py
@@ -0,0 +1,24 @@
+def normalize(name):
+    normalName = ""
+    if isinstance(name, str):
+
+        num = 0
+        for n in name:
+            if num == 0:
+                normalName += n.upper()
+            else:
+                normalName += n.lower()
+            num += 1
+    return normalName
+
+
+L1 = ['adam', 'LISA', 'barT']
+print(list(map(normalize, L1)))
+
+# Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
+#
+# print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
+# if prod([3, 5, 7, 9]) == 945:
+#     print('测试成功!')
+# else:
+#     print('测试失败!')
