e42943ef685da739f6f3b0794fa997ea16dd1529
FreedomPeace <18658251543@163.com>
2019-08-31 16:58:35 +0800

    函数编程-高阶函reduce

--- FuncProgram/AdvanceFun.py
+++ FuncProgram/AdvanceFun.py
@@ -22,12 +22,13 @@ from collections import Iterable
 # Python提供的sum()函数可以接受一个list并求和，请编写一个prod()函数，可以接受一个list并利用reduce()求积：
 def prod(L):
     if not isinstance(L, Iterable):
-        print(L, "is not a iterable obj")
+        print("is not a iterable obj")
         return
 
     return reduce(lambda x, y: x * y, L)
 
 
+print(prod(33))
 print('3 * 5 * 7 * 9 =', prod([3, 5, 7, 9]))
 if prod([3, 5, 7, 9]) == 945:
     print('测试成功!')
